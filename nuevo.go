5518e1e3dc1ac484ee205352130869bf61304407
Robert Ruiz Ovelar <robertruiz@fortalezainmuebles.com>
2025-12-02 18:01:07 -0300

    se agrega codigo de hola mundo

--- nuevo.go
+++ nuevo.go
@@ -1,1 +1,11 @@
-hola mundo
+package main
+
+import (
+	"fmt"
+)
+
+// main es la función de entrada principal para el programa.
+func main() {
+	// fmt.Println imprime una línea de texto seguida de un salto de línea.
+	fmt.Println("¡Hola Mundo desde Go!")
+}
