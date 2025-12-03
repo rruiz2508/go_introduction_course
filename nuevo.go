809d6ebba727272def5d5113773683bf6c0a6db0
Robert Ruiz Ovelar <robertruiz@fortalezainmuebles.com>
2025-12-03 08:30:00 -0300

    aquí hago el commit desde goland

--- nuevo.go
+++ nuevo.go
@@ -9,4 +9,5 @@ func main() {
 	// fmt.Println imprime una línea de texto seguida de un salto de línea.
 	fmt.Println("¡Hola Mundo desde Go!")
 	fmt.Println("Estoy modificando este programa")
+	fmt.Println("Quiero saber donde se genera el binario!!")
 }
